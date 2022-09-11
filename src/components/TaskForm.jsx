46819b221da7c51a7d68b1b44e7cdc4826eb7a4d
codeScarabee <samzodiak@gmail.com>
2022-09-11 03:51:17 +0100

    fix two

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -6,6 +6,7 @@ import AppContext from '../Context/AppContext';
 function TaskForm() {
   const [addTask, setAddTask] = useState({ name: '', amount: '', unit: '' });
   const [isClicked, setIsClicked] = useState(false);
+  const [message, setMessage] = useState('');
   const { addNewTask } = useContext(AppContext);
 
   const inputStyle = {
@@ -27,53 +28,64 @@ function TaskForm() {
     e.preventDefault();
     const { name, amount } = addTask;
     if (name && amount) {
-      addTask.id = uuidv4();
-      addNewTask(addTask);
-      setAddTask({ name: '', amount: '', unit: '' });
+      if (name.length > 15) {
+        setMessage('Name should be 15 characters or less');
+      } else if (amount.length > 4) {
+        setMessage('Amount should be 9999 or less');
+      } else {
+        addTask.id = uuidv4();
+        addNewTask(addTask);
+        setAddTask({ name: '', amount: '', unit: '' });
+        setMessage('');
+      }
     } else {
       setIsClicked((prev) => !prev);
+      setMessage('');
     }
   };
 
   const data = ['Bread', 'Butter', 'Milk', 'Potatoes', 'Tomato', 'Oil', 'Eggs', 'Apples', 'Bananas'];
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="input-group" style={inputStyle}>
-        <input
-          list="data"
-          name="name"
-          value={addTask.name}
-          placeholder="What are you going to buy?"
-          onChange={handleChange}
-        />
-        <datalist id="data">
-          {data.map((op) => (
-            <option key={op}>{op}</option>
-          ))}
-        </datalist>
-        <input
-          type="text"
-          id="amount"
-          name="amount"
-          value={addTask.amount}
-          placeholder="Amount?"
-          onChange={handleChange}
-        />
-        <select name="unit" value={addTask.unit} onChange={handleChange}>
-          <option value="">--Unit--</option>
-          <option value="mg">mg</option>
-          <option value="g">g</option>
-          <option value="kg">kg</option>
-          <option value="oz">oz</option>
-          <option value="lb">lb</option>
-          <option value="ml">ml</option>
-          <option value="l">l</option>
-        </select>
-      </div>
-      <button type="submit" style={btnStyle}>
-        {isClicked ? <FaArrowAltCircleDown size={48} color="gold" /> : <FaPlusCircle size={48} color="gold" />}
-      </button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group" style={inputStyle}>
+          <input
+            list="data"
+            name="name"
+            value={addTask.name}
+            placeholder="What are you going to buy?"
+            onChange={handleChange}
+          />
+          <datalist id="data">
+            {data.map((op) => (
+              <option key={op}>{op}</option>
+            ))}
+          </datalist>
+          <input
+            type="text"
+            id="amount"
+            name="amount"
+            value={addTask.amount}
+            placeholder="Amount?"
+            onChange={handleChange}
+          />
+          <select name="unit" value={addTask.unit} onChange={handleChange}>
+            <option value="">--Unit--</option>
+            <option value="mg">mg</option>
+            <option value="g">g</option>
+            <option value="kg">kg</option>
+            <option value="oz">oz</option>
+            <option value="lb">lb</option>
+            <option value="ml">ml</option>
+            <option value="l">l</option>
+          </select>
+        </div>
+        <button type="submit" style={btnStyle}>
+          {isClicked ? <FaArrowAltCircleDown size={48} color="gold" /> : <FaPlusCircle size={48} color="gold" />}
+        </button>
+      </form>
+      {message && <div className="message">{message}</div>}
+    </>
   );
 }
 
